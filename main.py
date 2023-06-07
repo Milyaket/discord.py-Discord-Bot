92e35e70956f3f2f43587c4960ebb04d6d92e6ab
Milyaket <94801710+Milyaket@users.noreply.github.com>
2023-06-08 01:08:47 +0200

    .env

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord, os
+from discord.ext import commands
+
+# This bot made with discord.py 2.2.3 by Milyaket#9669
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+# All cog folders comes here
+cogs = ["Ticket_System"]
+
+@bot.event
+async def setup_hook():
+    for cog in cogs:
+        for file in os.listdir(cog):
+            if file.endswith(".py"):
+                await bot.load_extension(f"{cog}.{file[:-3]}")
+
+    print(bot.user.name + " is online.")
+
+# Run your bot
+bot.run(token=os.getenv("TOKEN"))
